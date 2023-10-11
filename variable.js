1ab07aef4999a798bf7acce89b31ea3296f9fdd5
Nisha <104305943+Nisha7838@users.noreply.github.com>
2023-10-11 16:59:25 +0530

    variable

---
+++ variable.js
@@ -0,0 +1,20 @@
+const email="nisha7838@gmail.com";
+var phone =7838760605;
+let name="nisha";
+
+//investigetion
+
+//console.log("without change: ",email);
+//email="kr.nisha@gmail.com";//this line gives error its not possible to change const value
+//console.log("with change: ",email);
+
+//console.log(" without change phone: ",phone);
+// console.log("without change name: ",name);
+
+phone=567348543;
+name="kajal";
+
+// console.log(" with change phone: ",phone);
+// console.log("with change name: ",name);
+console.table([phone,name,email]);
+//let is scoope varible and var is the varible ;
