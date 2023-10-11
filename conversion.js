a6596f41def3e9293319976f6a2c3b4947004888
Nisha <104305943+Nisha7838@users.noreply.github.com>
2023-10-11 23:23:26 +0530

    operation

---
+++ conversion.js
@@ -0,0 +1,25 @@
+// let score = "33abcd";
+// let score = null;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber =Number(score);//NaN not a number
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+/*NaN is special type of datatype */
+
+/*"33" => 33
+"33abc" => NaN
+*/
+
+/*let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);*/
+
+//***********OPERATION ***********/
+// let value = 3;
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);//depend on the firt letter which type datatype
+console.log(2 + 2 + "2");
+
