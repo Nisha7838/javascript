de37e6e0b87c184f2f98af0f84988abd8ca6ad2e
Nisha <104305943+Nisha7838@users.noreply.github.com>
2023-10-11 22:55:52 +0530

    datatypes

---
+++ datatype.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as newer vesrion of js
+// alert("hello");//we are using nodejs ,not brower
+
+// console.log(3+3); 
+// console.log("nisha"+"3");
+
+/*null =>standalon Value
+null means empty
+undefined=> value should not be assign till now
+symbol=>uniqueness
+
+object
+*/
+// let name="nisha";
+
+/* 
+console.log(typeof null);
+console.log(typeof undefined);null is typeof object but undefined is typeof 
+undefined*/
