4cf87e3b34b30f0e8bdd5546dbb366cf4e2f2311
Nisha <104305943+Nisha7838@users.noreply.github.com>
2023-12-16 16:32:43 +0530

    arrow function

--- function.js
+++ function.js
@@ -25,10 +25,51 @@
 // console.log(totalAmmount(10,20,30,40,60));
 
 
-let a = 10;
-const b = 20 ;
-var c = 30;//var will be ascessable from out of the scope which is wronge thats why we not used var
+// let a = 10;
+// const b = 20 ;
+// var c = 30;//var will be ascessable from out of the scope which is wronge thats why we not used var
 
-console.log(`the value of let : ${a} and the value of const ${b} and the value of var ${c}`)
+//console.log(`the value of let : ${a} and the value of const ${b} and the value of var ${c}`)
+
+// function scopes(){
+//     let x =100;
+//     let y =20;
+//     if(x==10){
+//         z=1000;
+//     }
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+
+// }
+
+// scopes();
 
 
+//IIFE immediately Invoked Function Expression
+
+//()() first prithese for the defination and second for the call
+//global scopes jo pollution hota ha usko hatane ke liye jo IIFE used karte ha
+// (function chai(){
+//    console.log("hello IIFE");
+// })()
+
+
+// const two = function (){
+//     console.log("variable function");
+// }
+
+// two();
+
+//convert into arrow function
+
+const Arrow = () => {
+    let x= 10;
+    console.log(this);
+}
+
+Arrow();
+//in the arrow function we can not used the this keyword
+
+
+//arrow function
